8d4d3afa6e3e3aaf0ea6ab9e036aba86145e8329
paulovitorccc <paulo.dantas@ccc.ufcg.edu.br>
2018-07-31 22:20:00 -0300

    teste configuracao postgres para o heroku

--- src/main/java/br/com/prematricula/ws/repository/AlunoRepository.java
+++ src/main/java/br/com/prematricula/ws/repository/AlunoRepository.java
@@ -1,9 +1,11 @@
 package br.com.prematricula.ws.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.prematricula.ws.model.Aluno;
 
+@Repository
 public interface AlunoRepository extends JpaRepository<Aluno,Integer> {
 
 }
